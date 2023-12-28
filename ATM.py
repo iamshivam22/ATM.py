2986bfe4ad2fc665da97fdecc8c7ff0f76e315f8
iamshivam22 <154966237+iamshivam22@users.noreply.github.com>
2023-12-28 10:44:38 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,218 @@
+# print("please put in your card")
+# print("please chooese your language")
+# a=int(input("press 1:- english and press 2:-hindi:-"))
+# if a==1:
+#     print("you are selected english launguage")
+#     print("select your accunt type")
+#     b=int(input("press 1:-saving account and press 2:-current account:-"))
+#     if b==1:
+#         print("welcome to your saving account")
+#         print("please choose this")
+#         c=int(input("press 1:- witherewal press 2:- diposit press 3:- reset pin:-"))      
+#         if c==1:
+#             print("you are selected whitherwal amount")
+#             d=int(input("enter the your whitherwal amaount:-"))
+#             e=int(input("enter the your pin:-"))
+#             if e==4345:
+#                 print("your tranascation is seccessfull:-",d)
+#                 print("please take out your card")
+#             else:
+#                 print("your pin is worng please put your correct pin")
+#                 db=int(input("enter your pin:-"))
+#                 if db==4345:
+#                     print("your whitherwal is seccessful:-",d)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again plesae take out your card")
+#         elif c==2:
+#             print("you are selected deposite amount")
+#             f=int(input("enter your deposit amount:-"))
+#             g=int(input("enter the your pin:-"))
+#             if g==4345:
+#                 print("your deposit amount seccessful:-",f)
+#             else:
+#                 print("you are put worng pin please put your correct pin")
+#                 gb=int(input("enter the pin:-"))
+#                 if gb==4345:
+#                     print("your deposit amount seccessful:-",f)
+#                     print("take out your card")
+#                 else:
+#                     print("please try again take out your card")
+#         elif c==3:
+#             print("you are selected reset your pin")
+#             h=int(input("enter your old pin:-"))
+#             if h==4345:
+#                 i=int(input("please enter your new pin:"))
+#                 if i==1111:
+#                     print("your pin reset seccessful your new pin is:-",i)
+#                     print("take out your card")
+#                 else:
+#                     print("try again please take your pin")
+#             else:
+#                 print("please put your correct pin")
+#                 hb=int(input("enter your pin:-"))
+#                 if hb==4345:
+#                     ib=int(input("enter new pin:-"))
+#                     if ib==1111:
+#                         print("your pin reset seccessful your new pin:-",ib)
+#                         print("please take out your card")
+#                     else:
+#                         print("try again please take out your pin")
+#                 else:
+#                     print("please take out your pin")
+#     elif b==2:
+#         print("welcome to your current account")
+#         print("please choose this")
+#         j=int(input("press 1:- witherewal press 2:- diposit press 3:-reset pin:-"))
+#         if j==1:
+#             print("you are selected whitherwal amount")
+#             k=int(input("enter your witherwal amaount:-"))
+#             l=int(input("enter your pin:-"))
+#             if l==2022:
+#                 print("seccessfull your whitherwal amount:-",k)
+#                 print("please take out your card")
+#             else:
+#                 print("your pin is wrong put your correct pin")
+#                 lb=int(input("enter your pin"))
+#                 if lb==2022:
+#                     print("seccessfull your whitherwal amount:-",k)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your card")
+#         elif j==2:
+#             print("you are select diposit amount")
+#             m=int(input("please enter your deposit amount:-"))
+#             n=int(input("enter your pin:-"))
+#             if n==5456:
+#                 print("seccessful your deposit amount:-",m)
+#                 print("please take out your card")
+#             else:
+#                 print("your pin is wrong please put your correct pin")
+#                 nb=int(input("enter your pin:-"))
+#                 if nb==5456:
+#                     print("seccessful your deposit amount:-",m)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your pin")
+#         elif j==3:
+#             print("you selected reset your pin")
+#             o=int(input("enter your old pin:-"))
+#             p=int(input("enter your new pin:-"))
+#             if o==2123:
+#                 print("your pin reset seccessfull and your new pin:-",p)
+#                 print("please take out your pin")
+#             else:
+#                 print("your pin is wrong please put your correct pin")
+#                 ob=int(input("enter your old pin:-"))
+#                 if ob==2123:
+#                     print("seccessfull reset your pin and your new pin:-",p)
+#                     print("please take out your card")
+# elif a==2:
+#     print("you are selected hindi launguage")
+#     print("select your accunt type")
+#     ba=int(input("press 1:- saving account press 2:- current account:-"))
+#     if ba==1:
+#         print("welcome to your saving account")
+#         print("please choose this")
+#         ca=int(input("press 1:- witherewal press 2:- diposit press 3:-reset pin:-"))  
+#         if ca==1:
+#             print("you are selected whitherwal amount")
+#             da=int(input("enter the your whitherwal amaount:-"))
+#             ea=int(input("enter the your pin:-"))
+#             if ea==4345:
+#                 print("your tranascation is seccessfull:-",da)
+#                 print("please Take out your card")
+#             else:
+#                 print("your pin is worng please put your correct pin")
+#                 ec=int(input("enter your pin:-"))
+#                 if ec==4345:
+#                     print("seccessful your witherawal amount:-",da)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your card")
+#         elif ca==2:
+#             print("you are selected deposite amount")
+#             fa=int(input("enter your deposit amount:-"))
+#             ga=int(input("enter the your pin:-"))
+#             if ga==4345:
+#                 print("your deposit amount seccessful:-",fa)
+#                 print("please take out your card")
+#             else:
+#                 print("you are put worng pin please put correct pin")
+#                 gc=int(input("enter your pin:-"))
+#                 if gc==4345:
+#                     print("seccessful your deposite amount:-",fa)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your card")
+#         elif ca==3:
+#             print("you are selected reset your pin")
+#             ha=int(input("enter your old pin:-"))
+#             if ha==4345:
+#                 ia=int(input("please enter your new pin:"))
+#                 print("your pin reset seccessful your new pin is:-",ia)
+#                 print("please take out your card")
+#             else:
+#                 print("please put your correct pin")
+#                 hc=int(input("enter the old pin:-"))
+#                 if hc==4345:
+#                     ic=int(input("enter your new pin"))
+#                     print("seccessful your reset pin your new pin:-",ic)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your card")
+#     elif ba==2:
+#         print("welcome to your current account")
+#         print("please choose this")
+#         ja=int(input("press 1:- witherewal press 2:- diposit press 3:-reset pin:-"))
+#         if ja==1:
+#             print("you are selected whitherwal amount")
+#             ka=int(input("enter your witherwal amaount:-"))
+#             la=int(input("enter your pin:-"))
+#             if la==2022:
+#                 print("seccessfull your whitherwal amount:-",ka)
+#                 print("please take out your card")
+#             else:
+#                 print("your pin is wrong please put your correct pin")
+#                 lc=int(input("enter your pin"))
+#                 if lc==2022:
+#                     print("seccessful your witherwal amount:-",ka)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your card")
+#         elif ja==2:
+#             print("you are select diposit amount")
+#             ma=int(input("please enter your deposit amount:-"))
+#             na=int(input("enter your pin:-"))
+#             if na==5456:
+#                 print("seccessful your deposit amount:-",ma)
+#                 print("please take out your card")
+#             else:
+#                 print("your pin is wrong please put your correct pin")
+#                 nc=int(input("enter your pin:-"))
+#                 if nc==5456:
+#                     print("seccessful your witherewal amount:-",ma)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your card")
+#         elif ja==3:
+#             print("you are selected reset your pin")
+#             oa=int(input("enter your old pin:-"))
+#             pa=int(input("enter your new pin:-"))
+#             if oa==2123:
+#                 print("your pin reset seccessfull and your new pin:-",pa)
+#                 print("please take out your card")
+#             else:
+#                 print("your pin is wrong please put your correct pin")
+#                 oc=int(input("enter your old pin:-"))
+#                 if oc==2123:
+#                     print("your pin reset seccessful your new pin:-",pa)
+#                     print("please take out your card")
+#                 else:
+#                     print("try again please take out your card")
+
+
+
+
+
+
